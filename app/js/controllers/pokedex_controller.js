fa8eac472b02d5c79381b3f80afc225bd9e68786
Steve <wfuller87@gmail.com>
2015-10-25 01:55:57 -0700

    Improve Search
    Add error when character cannot be found and lowercase input for better search quality

--- app/js/controllers/pokedex_controller.js
+++ app/js/controllers/pokedex_controller.js
@@ -1,22 +1,28 @@
 /**
  * Created by williamfuller on 10/21/15.
  */
-app.controller('pokedexCtrl', function($scope, $http){
+app.controller('pokedexCtrl', function($scope, $http, $timeout){
+
+  // making search input lowercase, create better search results
+  $scope.searchCharacter = '';
+  $scope.$watch('searchCharacter', function() {
+    $scope.searchCharacter = $scope.searchCharacter.toLowerCase().replace(/\s+/g,'');
+  });
+
 
   $scope.search = function(){
     // get pokemon details
     $http({
       method: 'GET',
-      url: 'http://pokeapi.co/api/v1/pokemon/' + $scope.searchPokemon
+      url: 'http://pokeapi.co/api/v1/pokemon/' + $scope.searchCharacter
     }).then(function successCallback(data) {
       $scope.characters = [data.data];
       console.log($scope.characters);
 
 
 
-    }, function errorCallback(response) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
+    }, function errorCallback(data) {
+      alert('we are sorry but we could not find what you are looking for');
     });
   }
 
