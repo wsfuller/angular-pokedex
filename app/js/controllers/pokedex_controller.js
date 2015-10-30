8d7a9654e33bd98c9c28fc04559c17eb1dddc7e3
William S Fuller <wfuller87@gmail.com>
2015-10-29 17:16:41 -0700

    Resource Updates
    Signed-off-by: William S Fuller <wfuller87@gmail.com>

--- app/js/controllers/pokedex_controller.js
+++ app/js/controllers/pokedex_controller.js
@@ -1,54 +1,47 @@
 /**
  * Created by williamfuller on 10/21/15.
  */
-app.controller('pokedexCtrl', function($scope, $http, $timeout){
-
-  // making search input lowercase, create better search results
-  //$scope.searchCharacter = '';
-  //$scope.$filter('searchCharacter', function() {
-  //
-  //});
+app.controller('pokedexCtrl', function($scope, searchCharacter){
 
   $scope.search = function(){
     // get pokemon details
-    var searchCharacter = $scope.searchCharacter.toLowerCase().replace(/\s+/g,'');
-    $http({
-      method: 'GET',
-      url: 'http://pokeapi.co/api/v1/pokemon/' + searchCharacter
-    }).then(function successCallback(response) {
-      $scope.characters = [response.data];
-      console.log($scope.characters);
-    }, function errorCallback(response) {
-      alert('we are sorry but we could not find what you are looking for');
+    var filteredSearch = $scope.searchCharacter.toLowerCase().replace(/\s+/g,'');
+
+    $scope.characters = [searchCharacter.get({
+        value : filteredSearch
+      }, function(){},
+      function(response){
+        if(response.status === 404){
+          alert('we are sorry but we could not find what you are looking for')
+        }
+      }
+    )];
+
+    var pokemonObject = searchCharacter.get({value: filteredSearch}, function(value, responseHeader) {
+      console.log('Pokemon Object: ', pokemonObject);
     });
+
   };
 
   $scope.randomCharacter = function(){
-    //console.log('random character');
 
     function singleRandom() {
       return Math.floor((Math.random() * 718) + 1);
     }
-    console.log(singleRandom());
 
-    $http({
-      method: 'GET',
-      url: 'http://pokeapi.co/api/v1/pokemon/' + singleRandom()
-    }).then(function successCallback(response) {
-      $scope.characters = [response.data];
-      console.log($scope.characters);
-    }, function errorCallback(response) {
-      alert('we are sorry but we could not find what you are looking for');
+    $scope.characters = [searchCharacter.get({value : singleRandom()}, function(){},
+      function(response){
+        if(response.status === 404){
+          alert('Oh no something has broken with our randomizer')
+        }
+      }
+    )];
+
+    var pokemonObject = searchCharacter.get({value: singleRandom()}, function(value, responseHeader) {
+      console.log('Pokemon Object: ', pokemonObject);
     });
 
 
   };
-
-
-
-
-
-
-
 
 });
--- app/js/resources/pokedex_resource.js
+++ app/js/resources/pokedex_resource.js
@@ -1,11 +1,11 @@
 /**
  * Created by williamfuller on 10/21/15.
  */
-app.factory('Pokemon', ['$resource', function ($resource){
-  return $resource('http://pokeapi.co/api/v1/pokemon/89', {},{
+app.factory('searchCharacter', function($resource){
+  return $resource('http://pokeapi.co/api/v1/pokemon/:value', {value: '@searchCharacter'},{
     'query': {
       method: 'GET',
-      isArray: false
+      isArray: true
     }
   });
-}]);
+});
