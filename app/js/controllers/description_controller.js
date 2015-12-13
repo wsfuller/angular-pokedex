89f4bd8c3ab0c5e13ea704c9878bb718d7afcfb9
Steve <wfuller87@gmail.com>
2015-12-12 16:12:21 -0800

    Descriptions added

---
+++ app/js/controllers/description_controller.js
@@ -0,0 +1,27 @@
+/**
+ * Created by williamfuller on 12/12/15.
+ */
+app.controller('descriptionCtrl', function($scope, $http, $timeout){
+
+  $timeout(function(){
+    $scope.pokemonDescriptionURL = $scope.$parent.pokemon.descriptions[0].resource_uri;
+
+    if($scope.pokemonDescriptionURL == 'undefined'){
+      alert('no Description found');
+    }
+
+    console.log('parent pokemon: ', $scope.pokemonDescriptionURL);
+
+
+    $http({
+      method: 'GET',
+      url: 'http://pokeapi.co/'+$scope.pokemonDescriptionURL
+    }).then(function(results){
+      console.log('results: ', results);
+      $scope.description = results.data.description;
+      console.log($scope.description);
+    });
+  }, 500);
+
+
+});
